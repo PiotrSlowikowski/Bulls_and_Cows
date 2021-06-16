6ca0e438646cf3b3a3faae1f80fb7107cbf7d62f
PiotrSlowikowski <peter.slowikowski@hotmail.com>
2021-06-16 11:05:36 +0200

    Run round method implemented

--- Bulls and Cows/task/src/bullscows/Main.java
+++ Bulls and Cows/task/src/bullscows/Main.java
@@ -10,24 +10,44 @@ public class Main {
     }
 
     private static void BullsAndCowsInit() {
-        int secretCode = 9305;
-        System.out.println(createSecretCode());
+        System.out.println("Please, enter the secret code's length:");
+        Scanner scanner = new Scanner(System.in);
+        int secretNumberCodeLength = scanner.nextInt();
+        String secretCode = createSecretCode(secretNumberCodeLength);
 
-//        checkCowsAndBulls(secretCode);
+        if (!secretCode.isEmpty()) {
+            roundRun(secretCode);
+        }
     }
 
-    private static String createSecretCode() {
+    private static void roundRun(String secretCode) {
+        Scanner scanner = new Scanner(System.in);
+        int turnCounter = 1;
+        System.out.println("Okay, let's start a game! Secret code: " + secretCode);
+        while (true) {
+            System.out.println("Turn " + turnCounter + ":");
+            String codeGuess = scanner.next();
+            String outcome = checkCowsAndBulls(secretCode, codeGuess);
+            System.out.println(outcome);
+            if (outcome.contains("Congratulations")) {
+                break;
+            }
+            turnCounter++;
+        }
+    }
+
+    private static String createSecretCode(int secretNumberCodeLength) {
 
         Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
-        int secretNumberCode = scanner.nextInt();
-        if (secretNumberCode > 10) {
-            return "Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.";
-        }
+
 
         String secretCode = "";
 
-        while (secretCode.length() < secretNumberCode) {
+        while (secretCode.length() < secretNumberCodeLength) {
+            if (secretNumberCodeLength > 10) {
+                System.out.println("Error: can't generate a secret number with a length of 11 because there aren't enough unique digits.");
+                break;
+            }
             String digit = Integer.toString(random.nextInt(10));
             if (secretCode.length() == 0 && digit.equals("0")) {
                 continue;
@@ -36,34 +56,35 @@ public class Main {
                 secretCode += digit;
             }
         }
-        return "The random secret number is "+Integer.parseInt(secretCode);
+        return secretCode;
     }
 
-    private static void checkCowsAndBulls(int secretCode) {
-        Scanner scanner = new Scanner(System.in);
-        String codeString = Integer.toString(secretCode);
-        String inputString = scanner.nextLine();
+    private static String checkCowsAndBulls(String secretCode, String codeGuess) {
         int bullsCounter = 0;
         int cowsCounter = 0;
 
-        for (int i = 0; i < codeString.length(); i++) {
-            for (int j = 0; j < codeString.length(); j++) {
-                if (inputString.charAt(i) == codeString.charAt(j) && i == j) {
+        for (int i = 0; i < secretCode.length(); i++) {
+            for (int j = 0; j < secretCode.length(); j++) {
+                if (codeGuess.charAt(i) == secretCode.charAt(j) && i == j) {
                     bullsCounter++;
-                } else if (inputString.charAt(i) == codeString.charAt(j)) {
+                    continue;
+                } else if (codeGuess.charAt(i) == secretCode.charAt(j)) {
                     cowsCounter++;
                 }
             }
         }
 
-        if (cowsCounter > 0 && bullsCounter == 0) {
-            System.out.format("Grade: %s cow(s). The secret code is " + secretCode + ".", cowsCounter);
+        if (bullsCounter == secretCode.length()) {
+            System.out.println(bullsCounter + " bulls");
+            return "Congratulations! You guessed the secret code.";
+        } else if (cowsCounter > 0 && bullsCounter == 0) {
+            return "Grade: " + cowsCounter + " cow(s). The secret code is " + secretCode + ".";
         } else if (cowsCounter == 0 && bullsCounter > 0) {
-            System.out.format("Grade: %s bulls(s). The secret code is " + secretCode + ".", bullsCounter);
+            return "Grade: " + bullsCounter + " bulls(s). The secret code is " + secretCode + ".";
         } else if (cowsCounter > 0 && bullsCounter > 0) {
-            System.out.format("Grade: %s bull(s) and %s cow(s). The secret code is " + secretCode + ".", bullsCounter, cowsCounter);
+            return "Grade: " + bullsCounter + " bull(s) and " + cowsCounter + " cow(s). The secret code is " + secretCode + ".";
         } else {
-            System.out.format("Grade: None. The secret code is " + secretCode + ".");
+            return "Grade: None. The secret code is " + secretCode + ".";
         }
     }
 
